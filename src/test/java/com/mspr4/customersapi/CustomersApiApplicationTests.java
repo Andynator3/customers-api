09112f0a070a0bfb805904462716064732b3b1b7
Andy-Jeanty-Dev <andy.jeanty1@gmail.com>
2025-06-17 16:27:23 +0200

    Initial commit

---
+++ src/test/java/com/mspr4/customersapi/CustomersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mspr4.customersapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomersApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
