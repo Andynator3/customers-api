d41ee202b990cd2fa1cbc8378b9f20de776dac53
Andy-Jeanty-Dev <andy.jeanty1@gmail.com>
2025-09-03 02:21:38 +0200

    Changement de base de données de tests H2, pour la base de données de production PostgesSQL

--- src/main/java/com/mspr4/customersapi/entities/Customer.java
+++ src/main/java/com/mspr4/customersapi/entities/Customer.java
@@ -1,9 +1,6 @@
 package com.mspr4.customersapi.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,6 +9,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "customers")
 public class Customer {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
